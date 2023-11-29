9fe2c4d42fca515a03e5ebccd19714da4cc84392
geekelo <efueliht12@gmail.com>
2023-11-29 13:11:28 +0100

    Fix linter  errors

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
+# Top-level documentation for ApplicationMailer class
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
+# Top-level documentation for ApplicationMailer class
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
