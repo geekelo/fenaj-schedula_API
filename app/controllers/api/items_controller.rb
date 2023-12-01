c49261c364720f06173bd49f490d93bfa8a746da
joyce <joyce.ann.cruz@gmail.com>
2023-12-01 21:24:13 +0800

    Add controller index and show methods for items

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,12 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_api_session, only: %i[ show edit update destroy ]
+
+  # GET /api/items or /api/items.json
+  def index
+    @api_sessions = Api::Session.all
+  end
+
+  # GET /api/items/1 or /api/items/1.json
+  def show
+  end
 end
