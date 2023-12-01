a531e373df764c7b4174e5c4a174201eb83ddd43
Fatima Najafi <najafifatima762@gmail.com>
2023-12-01 10:00:37 +0430

    Create and migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_231_130_152_605) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_01_052136) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.string 'password_digest'
-    t.string 'username'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "reservations", force: :cascade do |t|
+    t.string "city"
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "session_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_reservations_on_session_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "image"
+    t.decimal "deposit"
+    t.decimal "spa_session_fee"
+    t.decimal "registration_fee"
+    t.decimal "total_amount_payable"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "reservations", "sessions"
+  add_foreign_key "reservations", "users"
 end
