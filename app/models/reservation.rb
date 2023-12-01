bca05fa5fb08d1d708bbf58eb0a033c5cbc86054
Fatima Najafi <najafifatima762@gmail.com>
2023-12-01 10:41:33 +0430

    Add validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :session
+
+  validates :user, presence: true
+  validates :course, presence: true
+  validates :city, presence: true
+  validates :date, presence: true
 end
