dd71e38d474b9a4d165ba1a7179cac74fe438596
Fatima Najafi <najafifatima762@gmail.com>
2023-12-01 12:10:31 +0430

    Update the user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :reservations
-  has_many :courses, through: :reservations
+  has_many :sessions, through: :reservations
 
   validates_presence_of :email
   validates_uniqueness_of :email
