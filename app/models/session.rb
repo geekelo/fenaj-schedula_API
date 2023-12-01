a36de0ea312eb599c6d29b54197d1c7515a9e6b6
Fatima Najafi <najafifatima762@gmail.com>
2023-12-01 10:01:02 +0430

    Add associations for all models

---
+++ app/models/session.rb
@@ -0,0 +1,8 @@
+class Session < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+  has_many :users, through: :reservations
+
+  has_one_attached :image
+  
+  validates :name, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :reservations
+  has_many :courses, through: :reservations
 
   validates_presence_of :email
   validates_uniqueness_of :email
