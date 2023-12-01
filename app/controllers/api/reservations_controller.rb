894aa1dcde602397dd3b3d010d0b99c2501ea019
Nasir <nasirbhatti.nk@gmail.com>
2023-12-01 21:34:37 +0500

    Add create method to create reservations

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -21,9 +21,22 @@ class Api::ReservationsController < ApplicationController
     }
   end
 
+  def create 
+    reservation = Reservation.new(reservation_params)
+
+    if reservation.save 
+      render json: { message: 'Reservation has been successfully created.'}, status: :ok
+      
+      item = Item.find_by(id: reservation_params[:item_id])
+      item.update(available: false) if item.present?
+    else
+      render json: {message: 'ERROR: unable to create reservation.'}, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def reservation_params
-    params.require(:reservation).permit(:city, :date, :user, :item)
+    params.require(:reservation).permit(:city, :date, :user_id, :item_id)
   end
 end
