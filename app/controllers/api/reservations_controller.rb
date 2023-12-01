de991813ea4d9a384284c93ba65910864a9ac88b
joyce <joyce.ann.cruz@gmail.com>
2023-12-01 21:25:00 +0800

    Add controller index and show methods for reservations

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,2 +1,10 @@
 class Api::ReservationsController < ApplicationController
+  # GET /api/reservations or /api/reservations.json
+  def index
+    @api_sessions = Api::Session.all
+  end
+
+  # GET /api/reservations/1 or /api/reservations/1.json
+  def show
+  end
 end
