1a748831e7f0129395aea15c605faaa5b4c5a8f6
joyce <joyce.ann.cruz@gmail.com>
2023-12-01 21:35:45 +0800

    Fix linter errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,12 +8,9 @@ class RegistrationsController < ApplicationController
 
     if user
       session[:user_id] = user.id
-      render json: {
-        status: :created,
-        user
-      }
+      render json: { status: :created, user }
     else
-      render json: { staus: 500 }
+      render json: { status: 500 }
     end
   end
 end
