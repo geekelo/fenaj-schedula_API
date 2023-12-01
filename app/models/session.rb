0c74a361ae0727db154b765efb0c209e1c419d2c
Fatima Najafi <najafifatima762@gmail.com>
2023-12-01 10:41:08 +0430

    Validate the attributes

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,4 +5,9 @@ class Session < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true
+  validates :description, presence: true
+  validates :spa_session_fee, presence: true, numericality: { greater_than: 0 }
+  validates :registration_fee, presence: true, numericality: { greater_than: 0 }
+  validates :total_amount_payable, presence: true, numericality: { greater_than: 0 }
+  validates :duration, presence: true
 end
