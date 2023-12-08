06f6d300084c9d7f1a372d50ee76642ed318af8a
Fatima Najafi <najafifatima762@gmail.com>
2023-12-08 22:48:26 +0430

    Setup the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +10,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/signup', to: 'registration#create'
       post '/login', to: 'authentication#create'
+      resources :items
+      # # resources :users
+      # resources :reservations
     end
   end
   # Defines the root path route ("/")
