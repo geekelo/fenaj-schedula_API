6cec663e5ac09977476b11a2eefc4018019de508
joyce <joyce.ann.cruz@gmail.com>
2023-12-08 23:52:34 +0800

    Add models and migration files for items and reservation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  has_many :reservations
+  has_many :items
+  has_many :items, through: :reservations
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_08_144021) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_08_153740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "image"
+    t.decimal "deposit"
+    t.decimal "spa_session_fee"
+    t.decimal "registration_fee"
+    t.decimal "total_amount_payable"
+    t.integer "duration"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "city"
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_reservations_on_item_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_08_144021) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "users"
+  add_foreign_key "reservations", "items"
+  add_foreign_key "reservations", "users"
 end
