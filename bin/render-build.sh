39a752ba3b3cddd0880fefcaa7bf34e040450f49
geekelo <efueliht12@gmail.com>
2023-12-15 01:21:18 +0100

    deploying

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,6 @@
 set -o errexit
 
 bundle install
-./bin/rails assets:precompile
-./bin/rails rake assets:clean
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
