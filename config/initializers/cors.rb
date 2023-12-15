11ab1d268d6b8ecec5b75b44ef39dbae22ac7e02
geekelo <efueliht12@gmail.com>
2023-12-15 09:21:47 +0100

    deploying

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000'
+      origins 'https://fenaj-schedula.onrender.com/'
   
       resource '*',
                headers: :any,
