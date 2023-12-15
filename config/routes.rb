9758de64fe264a96e11118bf73d27ff809f1a1fd
geekelo <efueliht12@gmail.com>
2023-12-15 20:04:46 +0100

    Deploying

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  get '/openapi.json', to: 'swagger#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
