f244e974be6d1651b95ba48e890ced61e3f39e04
geekelo <efueliht12@gmail.com>
2023-12-18 09:34:13 +0100

    🏂 Implemented controller methods

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,6 +6,20 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+# config/initializers/cors.rb
+
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'https://fenaj-schedula.onrender.com' # Add your frontend origin(s)
+
+#     resource '/api/*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true
+#   end
+# end
+
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'http://localhost:3000'
